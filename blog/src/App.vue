8a8ed9e3b0d1e42426fb453f094d940fe9b7e52f
Luchs41 <kokobo6@gmail.com>
2021-08-13 12:43:33 +0900

    List.vue 어떻게해결하지

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">My Vuelog</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -22,7 +22,7 @@
 </nav>
 
 
-<router-link to="/list">리스트</router-link>
+<router-link to="/list">리스트</router-link> &nbsp;
 <router-link to="/">홈으로</router-link>
 
 <div class="mt-4">
--- blog/src/components/List.vue
+++ blog/src/components/List.vue
@@ -1,6 +1,6 @@
 <template>
 	<div v-for="(a, i) in 블로그글" :key="i">
-		<router-link :to="detail/i">{{블로그글[i].title}}</router-link>
+		<router-link :to="`detail/${i}`">{{블로그글[i].title}}</router-link>
 		<p>{{블로그글[i].date}}</p>
 		<p>{{블로그글[i].content}}</p>
 	</div>
